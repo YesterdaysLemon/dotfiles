d48d1b1d6964b585985e00c9f1d31ba84363bdbb
Alireza Afshan <yesterdayslemon@yahoo.com>
2025-01-22 22:08:04 +0300

    gotta love gruvbox - wezterm

--- dot-config/wezterm/wezterm.lua
+++ dot-config/wezterm/wezterm.lua
@@ -1,7 +1,11 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Monokai Pro (Gogh)"
+-- config.color_scheme = "Monokai Pro (Gogh)"
+-- config.color_scheme = "Gogh (Gogh)"
+-- config.color_scheme = "Gruvbox (Gogh)"
+config.color_scheme = "Gruvbox dark, hard (base16)"
+-- config.color_scheme = "Gruvbox Material (Gogh)"
 
 config.hide_tab_bar_if_only_one_tab = true
 config.exit_behavior = "Close"
@@ -10,8 +14,8 @@ config.adjust_window_size_when_changing_font_size = false
 config.window_padding = {
 	left = 10,
 	right = 10,
-	top = 5,
-	bottom = 5,
+	top = 10,
+	bottom = 10,
 }
 
 --window
@@ -21,7 +25,7 @@ config.inactive_pane_hsb = {
 	brightness = 0.8,
 }
 --fonts
-config.font_size = 16
+config.font_size = 19
 config.line_height = 1.2
 
 return config
